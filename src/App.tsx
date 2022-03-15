7ca75457b44b3faeec4053344b7362afbd42e132
Gerardo-Hu89 <gerardo.hu89@gmail.com>
2022-03-15 11:24:18 -0600

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,8 @@
 import React from 'react';
-import './App.css';
 import { Main } from './onePirate';
 
 function App() {
-  return (
-    <>
-      <Main />
-    </>
-  );
+  return <Main />;
 }
 
 export default App;
