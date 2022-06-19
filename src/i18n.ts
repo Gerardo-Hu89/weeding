d2eec9f8f23af8a386e607f8d88316f9db1449e5
Gerardo Huerta <jhuerta@lean-tech.io>
2022-06-19 02:34:53 -0500

    2

---
+++ src/i18n.ts
@@ -0,0 +1,15 @@
+import i18n from "i18next";
+import { languages } from "./resources/languages";
+import { initReactI18next } from "react-i18next";
+
+i18n
+  .use(initReactI18next)
+  .init({
+    resources: languages,
+    lng: "en",
+    interpolation: {
+      escapeValue: false
+    }
+  });
+
+  export default i18n;
