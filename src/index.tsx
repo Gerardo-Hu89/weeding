c4eb46619c27d1aab9e08ae225587292cc4c1b59
Gerardo Huerta <jhuerta@lean-tech.io>
2022-06-19 02:41:42 -0500

    3

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,11 @@
 import React from 'react';
 import App from './App';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
 import './i18n';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
       <App />
-    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
